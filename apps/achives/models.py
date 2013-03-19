4c03dac6a76fc2321c36ac25e99963174ef3a00a
zerc_army <zero13cool@yandex.ru>
2013-03-19 21:02:16 +0400

    first working (i hope :) version of models

--- apps/achives/models.py
+++ apps/achives/models.py
@@ -14,74 +14,112 @@ class Achive(BaseModel):
         'icon'       : unicode,
         'score'      : float,
         'trick_id'   : int,                        
-        'rule'       : {
-            'cones'        : [], # диапазоны конусов 
-            'elements'     : {      # для ачивки с чек-листом
-                'collection'      : unicode,
-                'collection_ids'  : []
-            },
-        },
-        'parents'    : [int],        
+        'rule'       : {unicode: []},
+        'parents'    : [], 
     }
     default_values  = {'score': 1.0, 'parents': []}
     required_fields = ['title', 'descr']
     #indexes = [{'fields': ['trick_id']}]
 
+
     ### some methods
-
-    # testing value
-    def test_cones(self, value):
-        conditions = self.get('rule')['cones']
-        test_result = False
-        return value > min(conditions)
-
-    def test_elements(self, value_dict):
-        result = value_dict['collection'] == self.get('rule')['elements']['collection']
-        if not result: return
-
-        ids = set(value_dict['collection_ids'])
-        diff = set(self.get('rule')['elements']['collection_ids']) - ids
-        return diff == ids
-
-    def test(self, rule_name, value):
-        """
-        Тестирует value согласно правилу rule.<field_name>
-        """
+    def status_event(self, value):
+        rule_name, condition = self.get('rule').items()[0]
         try:
-            return getattr(self, 'test_%s' % rule_name)(value)
+            return getattr(self, '_status_event_%s' % rule_name)(value)
         except AttributeError:
             print u'Unknow rule: %s' % rule_name
         return False
 
-    def get_event(self, user_id):
-        event = app.connection.Event.fetch_one({"user_id": user_id, "achive_id": self.get("_id")})
+    def _status_event_cones(self, value):
+        index = self.get_level(value) - 1
+        condition = self.get('rule')['cones']
+        return value >= condition[index]        
+
+    def _status_event_complex(self, value):
+        condition = self.get('rule')['complex']
+        return len(condition) == len(value)
+
+    # testing
+    def test(self, value):
+        rule_name, condition = self.get('rule').items()[0]
+        try:
+            return getattr(self, '_test_%s' % rule_name)(value)
+        except AttributeError:
+            print u'Unknow rule: %s' % rule_name
+        return False
+
+    def _test_cones(self, value):
+        return not not self._level_for_cones(value)
+
+    def _test_complex(self, value):
+        original   = set(self.get('rule')['complex'])
+        value      = set(value)
+        print original, value
+        return not value - original
         
+    # level
+    def get_level(self, value):
+        """
+        Определяет уровень на который пользователь выполнил ачивку
+        """
+        rule_name, condition = self.get('rule').items()[0]
+        try:
+            return getattr(self, '_level_for_%s' % rule_name)(value)
+        except AttributeError:
+            print u'Unknow rule_name: %s' % rule_name
+        return 0    
+
+    def _level_for_cones(self, value):
+        conditions, level = self.get('rule')['cones'], 1        
+
+        for c in conditions:
+            if value <= c or level == len(conditions):
+                break
+            level += 1
+
+        return level
+
+    def _level_for_complex(self, value):
+        # берем по лвл дочерней ачивки
+        pass
+
+    def get_event(self, user_id, level):
+        event = app.connection.Event.fetch_one({"user_id": user_id, "achive_id": self.get("_id"), "level": level})
+
         if not event:
             event = app.connection.Event()
-            event.update({"user_id": user_id, "achive_id": self.get("_id")})
+            event.update({"user_id": user_id, "achive_id": self.get("_id"), "level": level})
 
         return event
 
-    def make_event(self, user_id, rule_name, value):
+    def make_event(self, user_id, value):
         """
         Регистрирует достижение пользователя
         """
-        if not self.test(rule_name, value): return
-
-        event = self.get_event(user_id)
-        event.get('progress').update({rule_name: value})
+        if not self.test(value): return False
+        
+        level = self.get_level(value)
+        event = self.get_event(user_id, level)
+        event.update({'progress': [value], 'done': self.status_event(value)})
         event.save()
 
-
-    def update_parents(self, user_id):
+    def update_parents(self, user_id, level):
         """
         Обновляет данные по дереву родителей
         """
         parents = app.connection.Achive.fetch({"_id": {"$in": self.get('parents')}})
+
         for p in parents:
-            event = p.get_event(user_id)
-            event.get('progress', []).append(self.get("_id"))
+            event = p.get_event(user_id, level)
+            event.update({'level': level})
+
+            current_progress = event.get('progress', [])
+            current_progress.append(self.get("_id"))
+            current_progress = list(set(current_progress))
+            event.update({'progress': current_progress, 'done': p.status_event(current_progress)})
             event.save()
+
 app.connection.register([Achive])
 app.db.seqs.insert({'_id': 'achives_seq',  'val': 0})
 
@@ -95,23 +133,20 @@ class Event(BaseModel):
         'time_changed' : datetime,
         'level'        : int,
         'done'         : bool,
-        'progress'     : {
-            'cones'        : int,   
-            'elements'     : {
-                'collection'      : unicode,
-                'collection_ids'  : []
-            },
-        },
+        'progress'     : []
     }
-    default_values = {'done': False, 'level': 0, 'time_changed': datetime.now}
+    default_values  = {'done': False, 'level': 0, 'time_changed': datetime.now}
     required_fields = ['user_id']
-    indexes = [{'fields': ['user_id', 'achive_id']}]
+    indexes         = [{'fields': ['user_id', 'achive_id']}]
+
 app.connection.register([Event])
 app.db.seqs.insert({'_id': 'events_seq',  'val': 0})
 
 
 
 ### исключительно для теста :)
+    
+
 def fill():
     achives = (
         {
@@ -119,7 +154,7 @@ def fill():
             'trick_id'   : 7,
             'descr'      : u'Выполните трюк на 4,20,40 конусов',
             'icon'       : u'0.jpg',            
-            'rule'   : {u'cones': [4,20,40], u'elements': {'collection': u'', 'collection_ids':[]}},
+            'rule'       : {u'cones': [4,20,40]},
             'parents'    : [2],
         },
         {
@@ -127,7 +162,7 @@ def fill():
             'trick_id'   : 13,
             'descr'      : u'Выполните трюк на 4,20,40 конусов',
             'icon'       : u'0.jpg',
-            'rule'   : {u'cones': [4,20,40],  u'elements': {'collection': u'', 'collection_ids':[]}},
+            'rule'       : {u'cones': [4,20,40]},
             'parents'    : [2],
         },
         {
@@ -135,13 +170,7 @@ def fill():
             'descr'      : u'Получите достижения OneWheel forward и OneWheel backward.',
             'icon'       : u'0.jpg',
             'score'      : 2.0,
-            'rule'   : {
-                'cones': [],
-                'elements': {
-                    'collection'      : u'achive',
-                    'collection_ids'  : [0, 1]
-                }
-            }
+            'rule'       : {u'complex': [0, 1]}            
         }
     )
 
@@ -150,3 +179,6 @@ def fill():
         a['_id'] = app.db.seqs.find_and_modify({"_id": "achives_seq"}, {"$inc": {"val": 1}})['val']
         achive.update(a)
         achive.save()
+
+achive = app.connection.Achive.fetch({"_id": 1})[0]
+test_value, user_id = 10, 0
---
+++ apps/achives/tests.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+"""
+    apps.achives.tests
+    ~~~~~~~~~~~
+
+    Тесты для ачивок
+
+    :copyright: (c) 2013 by zero13cool
+"""
+import simplejson as json
+import unittest
+
+from flask import get_flashed_messages
+
+from apps.tests import BaseTestCase
+
+class TricksTestCase(BaseTestCase):
+    pass
